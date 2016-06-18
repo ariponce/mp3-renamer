d0feca4d38170c34fe252cbae4f83390ed8a3ab9
ariponce <aponce@protonmail.com>
2016-06-18 14:15:19 +0200

    Added option to also rename dir + better error handling

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.optflag("h", "help", "Print help menu");
+    opts.optflag("", "rename-dir", "Also rename the directory where files are located");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
@@ -35,6 +36,10 @@ fn main() {
 
     if path.is_dir() {
         parse_dir(path);
+
+        if matches.opt_present("rename-dir") {
+            rename_dir(path);
+        }
     } else {
         match parse_file(path) {
             Ok(_) => println!("Done"),
@@ -52,6 +57,7 @@ fn parse_dir(path: &Path) {
             Err(e) => println!("Failed to process {:?}: {}", path.file_name().unwrap(), e)
         }
     }
+
 }
 
 fn parse_file(path: &Path) -> Result<(), String> {
@@ -59,13 +65,18 @@ fn parse_file(path: &Path) -> Result<(), String> {
     if extension != "mp3" {
         return Err("Not an mp3 file".to_string());
     }
-    let tag = Tag::read_from_path(path).unwrap();
+
+	let tag = match Tag::read_from_path(path) {
+		Ok(v) => { v },
+		Err(e) => { return Err(e.to_string()) }
+	};
     let mut new_path = PathBuf::from(path);
     let song = tag.title().unwrap();
     let mut track_number: String = String::from("");
     let track = tag.track().unwrap().to_string();
-    if !track.starts_with("0") || !track.len() >= 2 {
-        let mut track_number:String = "0".to_owned();
+    // Add a leading zero for track between 1-9
+    if track.len() < 2 {
+        track_number = "0".to_owned();
     }
     track_number.push_str(&track);
     let new_filename = track_number + " - " + song + "." + extension.to_str().unwrap();
@@ -77,6 +88,24 @@ fn parse_file(path: &Path) -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn rename_dir(path: &Path) -> Result<(), String> {
+   let file = fs::read_dir(path).unwrap().next().unwrap().unwrap().path();
+   if file.extension().unwrap() == "mp3" {
+	   let tag = Tag::read_from_path(file).unwrap();
+       let year = tag.year().unwrap().to_string();
+       let album = tag.album().unwrap();
+       let mut new_path = PathBuf::from(path);
+       new_path.set_file_name(year + " - " + album);
+
+       match fs::rename(path, new_path) {
+           Ok(_) => (),
+           Err(e) => return Err(e.to_string()),
+       }
+   }
+
+   Ok(())
 }
 
 fn print_usage(program: &str, opts: Options) {
